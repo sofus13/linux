2e82c54bf959812340ef192afb181b0ddd48ba81
James Calligeros <jcalligeros99@gmail.com>
2026-07-26 20:04:50 +1000

    HACK: drm: apple: do not destroy stale framebuffer references
    Because we have not yet figured out how to clear surfaces, freeing old
    framebuffer references crashes DCP with IOVA errors. Don't destroy them
    for now so that we can continue working.
    
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- drivers/gpu/drm/apple/iomfb_template.c
+++ drivers/gpu/drm/apple/iomfb_template.c
@@ -738,6 +738,8 @@ static void dcp_swap_cleared(struct apple_dcp *dcp, void *data, void *cookie)
 		return;
 	}
 
+	/* TODO: Figure out how to clear surfaces for 14.x */
+#if DCP_FW_VER < DCP_FW_VERSION(14, 7, 0)
 	while (!list_empty(&dcp->swapped_out_fbs)) {
 		struct dcp_fb_reference *entry;
 		entry = list_first_entry(&dcp->swapped_out_fbs,
@@ -749,6 +751,7 @@ static void dcp_swap_cleared(struct apple_dcp *dcp, void *data, void *cookie)
 		list_del(&entry->head);
 		kfree(entry);
 	}
+#endif
 }
 
 static void dcp_swap_clear_started(struct apple_dcp *dcp, void *data,
@@ -1145,6 +1148,8 @@ static void dcp_swapped(struct apple_dcp *dcp, void *data, void *cookie)
 	}
 	dcp->swap_start = ktime_get();
 
+	/* TODO: Figure out how to clear surfaces on 14.x */
+#if DCP_FW_VER < DCP_FW_VERSION(14, 7, 0)
 	while (!list_empty(&dcp->swapped_out_fbs)) {
 		struct dcp_fb_reference *entry;
 		entry = list_first_entry(&dcp->swapped_out_fbs,
@@ -1156,6 +1161,7 @@ static void dcp_swapped(struct apple_dcp *dcp, void *data, void *cookie)
 		list_del(&entry->head);
 		kfree(entry);
 	}
+#endif
 }
 
 static void dcp_swap_started(struct apple_dcp *dcp, void *data, void *cookie)
